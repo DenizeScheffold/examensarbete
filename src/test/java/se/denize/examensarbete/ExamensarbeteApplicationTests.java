170a8eed704bc3fdbfdf641914c284921fae160b
DenizeScheffold <denizescheffold@gmail.com>
2022-12-20 14:29:04 +0100

    setup tests.

--- src/test/java/se/denize/examensarbete/ExamensarbeteApplicationTests.java
+++ src/test/java/se/denize/examensarbete/ExamensarbeteApplicationTests.java
@@ -2,12 +2,21 @@ package se.denize.examensarbete;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import se.denize.examensarbete.model.User;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ExamensarbeteApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void doesUserHaveName(){
+		User user = new User("Stina");
+		assertNotNull(user);
 	}
 
 }
