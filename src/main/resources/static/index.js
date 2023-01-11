3c9349574d92aff6c9a32e8acc509f90e438e65d
DenizeScheffold <denizescheffold@gmail.com>
2023-01-11 10:35:25 +0100

    remove implementation of thymeleaf

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,30 +1,44 @@
 //TODO make all child element to change color.
 function buttonColor() {
-    let element =  document.getElementById("button-change-color");
-    if (element.style.backgroundColor == "green") {
-       element.style.backgroundColor = "red";
+    let elementML = document.getElementById("button-change-color-monday-L");
+    if (elementML.style.backgroundColor === "green") {
+        elementML.style.backgroundColor = "red";
     } else {
-        element.style.backgroundColor = "green";
+        elementML.style.backgroundColor = "green";
     }
+    return elementML;
+}
 
+function buttonColorMH() {
+    let elementMH = document.getElementById("button-change-color-monday-H");
+    if (elementMH.style.backgroundColor === "green") {
+        elementMH.style.backgroundColor = "red";
+    } else {
+        elementMH.style.backgroundColor = "green";
+    }
+    return elementMH;
 }
 
 //TODO save input to JSON file
-function onSubmit() {
-
+function onSubmit(elementML, elementMH) {
+    const elements = {elementML, elementMH};
+    JSON.stringify(elements);
+    console.log(elements);
+    /*
     let planToSubmit = document.getElementsByClassName('grid-child-element')[0];
-    if (document.getElementById("button-change-color").style.backgroundColor == "green"){
+    if (document.getElementById("button-change-color").style.backgroundColor === "green"){
         const mondayLGreen = planToSubmit;
         console.log("L possible", mondayLGreen);
     }
     else{
         const mondayLRed = planToSubmit;
         console.log(mondayLRed);
-
     }
-
-
+*/
 }
+
+
+
 
 
 
