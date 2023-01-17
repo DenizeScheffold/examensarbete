85effdf7b966b502b2f465aac101b2aa86f878d6
DenizeScheffold <denizescheffold@gmail.com>
2023-01-17 14:13:52 +0100

    further spring security

---
+++ src/main/java/se/denize/examensarbete/login/TestController.java
@@ -0,0 +1,47 @@
+package se.denize.examensarbete.login;
+/*
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class TestController {
+
+
+    private final AppPasswordConfig bcrypt;
+
+    @Autowired
+    public TestController(AppPasswordConfig bcrypt) {
+        this.bcrypt = bcrypt;
+    }
+
+
+    @GetMapping("/encode")
+    public String testEncoding() {
+        return bcrypt.bCryptPasswordEncoder().encode("password");
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String testAdminPermission() {
+        return "only admins can enter";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public String testUserPermission() {
+        return "only users can enter";
+    }
+
+    @GetMapping("/unknown")
+    @PreAuthorize("hasRole('ROLE_ADMIN')" + " && " + "hasAuthority('user:read')")
+    public String testUnknownPermission() {
+        return "this should never work";
+    }
+
+
+}
+        */
