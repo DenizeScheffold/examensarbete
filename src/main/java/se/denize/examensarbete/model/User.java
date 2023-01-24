f3d32d9ad7df6895c48b9b139e920e0dc43e62f9
DenizeScheffold <denizescheffold@gmail.com>
2023-01-24 11:07:35 +0100

    add credientials to user

--- src/main/java/se/denize/examensarbete/model/User.java
+++ src/main/java/se/denize/examensarbete/model/User.java
@@ -1,11 +1,15 @@
 package se.denize.examensarbete.model;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 import java.util.List;
 
 @Entity
-@Table(name="users")
-public class User {
+@Table(name = "users")
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id", nullable = false)
@@ -24,9 +28,27 @@ public class User {
     }
 
     public User() {
-
     }
-    public long getUserId(){
+
+    private String username;
+    private String password;
+    // TODO - FIX
+    // private Collection<? extends GrantedAuthority> authorities;
+    private boolean isAccountNonExpired;
+    private boolean isAccountNonLocked;
+    private boolean isCredentialsNonExpired;
+    private boolean isEnabled;
+
+    public User(String username, String password, boolean isAccountNonExpired, boolean isAccountNonLocked, boolean isCredentialsNonExpired, boolean isEnabled) {
+        this.username = username;
+        this.password = password;
+        this.isAccountNonExpired = isAccountNonExpired;
+        this.isAccountNonLocked = isAccountNonLocked;
+        this.isCredentialsNonExpired = isCredentialsNonExpired;
+        this.isEnabled = isEnabled;
+    }
+
+    public long getUserId() {
         return userId;
     }
 
@@ -42,11 +64,42 @@ public class User {
         this.otherParentId = otherParentId;
     }
 
-
-
     public long getOtherParentId() {
         return otherParentId;
     }
 
+    @Override
+    public String getUsername() {
+        return username;
+    }
 
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
 }
